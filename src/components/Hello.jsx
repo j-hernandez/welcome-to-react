e18719503099d729a4d11cfcce32f9515c79aceb
James Hernandez <james@j-hernandez.com>
2021-01-14 19:41:49 -0500

    es5 refactor

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import './Hello.css';
 
-class Hello extends React.Component {
-
-    // Say Hello World from in here
-
-    render() {
-        return (
-            <h1 className='title'>
-                Hello {this.props.name} from our new Component
-
-            </h1>
-        )
-    }
-
+function Hello(props) {
+    return (
+        <h1 className='title'>
+            Hello {props.name} from our new Component
+        </h1>
+    )
 }
 
 export default Hello;
